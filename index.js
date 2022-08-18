9a8270ab66f202c35ce150fff3d09e68ede12a43
vitorlc <vitor_cirqueira1@hotmail.com>
2022-08-18 14:55:55 -0300

    feat: add npx support

---
+++ index.js
@@ -0,0 +1,9 @@
+const { spawn } = require('child_process')
+const pythonProcess = spawn('python3', ['./check-git-repositories.py'])
+
+pythonProcess.stdout.on('data', data => {
+  console.log(data.toString())
+})
+pythonProcess.stderr.on('data', data => {
+  console.log(data.toString())
+})
